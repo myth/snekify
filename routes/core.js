5ec1f4ced65156a4c5505d0a4ae294fe422e8803
myth <myth@overflow.no>
2016-03-16 05:08:32 +0100

    Remove unneeded deps

--- routes/core.js
+++ routes/core.js
@@ -5,15 +5,9 @@
 */
 
 var express = require('express')
-var bp = require('body-parser')
-var router = express.Router()
-
-// Base handler
-router.use(bp.json({}))
-router.use(bp.urlencoded({extended: false}))
 
 router.route('/').get(function (req, res, next) {
-  res.render('index', { title: 'Snekify' })
+  req.send('It works')
 })
 
 module.exports = router
